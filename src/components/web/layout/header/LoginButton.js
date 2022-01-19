6a81723d2ade2043a6661bd7675b6fd72401ed9b
Asif-Hassan-Rahul <63399934+Asif-Hassan-Rahul@users.noreply.github.com>
2022-01-20 05:28:00 +0600

    ...

--- src/components/web/layout/header/LoginButton.js
+++ src/components/web/layout/header/LoginButton.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 function LoginButton() {
-  return (
-    <>
-      <li>
-        <a className="nav-link scrollto" href="/login">
-          Login
-        </a>
-      </li>
-      <li>
-        <a className="nav-link scrollto" href="/register">
-          Register
-        </a>
-      </li>
-    </>
-  );
+    return (
+        <>
+            <li>
+                <a className="nav-link scrollto" href="/login">
+                    Login
+                </a>
+            </li>
+            <li>
+                <a className="nav-link scrollto" href="/register">
+                    Register
+                </a>
+            </li>
+        </>
+    );
 }
 
 export default LoginButton;
